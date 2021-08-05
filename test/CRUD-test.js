729d8e00f939d81db64f28598b3bc651f3d8aa89
Craig Banach <craigsb93@gmail.com>
2021-08-05 17:30:58 +0100

    Initial create smart contract with tests

--- test/CRUD-test.js
+++ test/CRUD-test.js
@@ -29,5 +29,24 @@ describe("CRUD", () => {
             assert.equal(post.text, "Second.", "contains the correct text");
             assert.equal(post.creator, addr1.address, "post is from the correct address");            
         })
+
+        it("can add multiple posts from the same account", async() => {
+            const [owner] = await ethers.getSigners();
+            const CRUD = await ethers.getContractFactory("CRUD");
+            const crud = await CRUD.deploy("First.");
+            await crud.deployed();
+
+            await crud.addPost("Second.");
+            const post1 = await crud.posts(1);
+            const post2 = await crud.posts(2);
+
+            assert.equal(post1.id, 1, "contains the correct id");
+            assert.equal(post1.text, "First.", "contains the correct text");
+            assert.equal(post1.creator, owner.address, "post is from creator address");
+
+            assert.equal(post2.id, 2, "contains the correct id");
+            assert.equal(post2.text, "Second.", "contains the correct text");
+            assert.equal(post2.creator, owner.address, "post is from creator address");
+        })
     })
 })
